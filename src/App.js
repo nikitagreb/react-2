c2979811bf0cda3efe08b807de39204c0e3c6465
Nikita Grebennikov <nikitagreb0204@mail.ru>
2021-09-27 22:15:37 +0300

    6.46

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, {Component} from 'react'
 import Layout from './hoc/Layout/Layout'
+import Quiz from './containers/Quiz/Quiz'
 
 class App extends Component{
   render() {
     return (
         <Layout>
-            <h1>Hello react 22</h1>
+            <Quiz />
         </Layout>
     );
   }
