0e02c699e1ec270c88f6839c08b84e2a0fb4c813
Nikita Grebennikov <nikitagreb0204@mail.ru>
2021-09-28 22:05:35 +0300

    6.52

--- src/containers/Quiz/Quiz.js
+++ src/containers/Quiz/Quiz.js
@@ -34,7 +34,13 @@ class Quiz extends Component {
     }
 
     onAnswerClickHandler = answerId => {
-        console.log('Answer Id: ', answerId)
+
+        if (this.state.answerState) {
+            const key = Object.keys(this.state.answerState)[0]
+            if (this.state.answerState[key] === 'success') {
+                return
+            }
+        }
 
         const question = this.state.quiz[this.state.activeQuestion]
 
