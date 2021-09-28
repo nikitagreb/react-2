2e12eaccf9f6b398d0ed21fa5acb9becd74296cb
Nikita Grebennikov <nikitagreb0204@mail.ru>
2021-09-28 22:01:28 +0300

    6.51

--- src/containers/Quiz/Quiz.js
+++ src/containers/Quiz/Quiz.js
@@ -6,6 +6,7 @@ class Quiz extends Component {
 
     state = {
         activeQuestion: 0,
+        answerState: null, // {[id] : 'success' | 'error'}
         quiz: [
             {
                 question: 'Какого цвета небо',
@@ -35,9 +36,39 @@ class Quiz extends Component {
     onAnswerClickHandler = answerId => {
         console.log('Answer Id: ', answerId)
 
-        this.setState({
-            activeQuestion: this.state.activeQuestion + 1
-        })
+        const question = this.state.quiz[this.state.activeQuestion]
+
+        if (question.rightAnswerId === answerId) {
+
+            this.setState({
+                answerState: {[answerId]: 'success'}
+            })
+
+            const timeout = window.setTimeout(() => {
+
+                if (this.isQuizFinished()) {
+
+                    console.log('finish')
+
+                } else {
+                    this.setState({
+                        activeQuestion: this.state.activeQuestion + 1,
+                        answerState: null
+                    })
+                }
+
+                window.clearTimeout(timeout)
+            }, 1000)
+
+        } else {
+            this.setState({
+                answerState: {[answerId]: 'error'}
+            })
+        }
+    }
+
+    isQuizFinished() {
+        return this.state.activeQuestion + 1 === this.state.quiz.length
     }
 
     render() {
@@ -51,6 +82,7 @@ class Quiz extends Component {
                         onAnswerClick={this.onAnswerClickHandler}
                         quizLenght={this.state.quiz.length}
                         answerNumber={this.state.activeQuestion + 1}
+                        state={this.state.answerState}
                     />
                 </div>
             </div>
