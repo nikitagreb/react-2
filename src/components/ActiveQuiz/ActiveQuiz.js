842fdf8b025442ee47fcbed583e6510187c345e9
Nikita Grebennikov <nikitagreb0204@mail.ru>
2021-09-28 09:03:33 +0300

    6.48

--- src/components/ActiveQuiz/ActiveQuiz.js
+++ src/components/ActiveQuiz/ActiveQuiz.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import classes from './ActiveQuiz.module.scss'
+import AnswersList from './AnswersList/AnswersList'
 
 const ActiveQuiz = props => (
     <div className={classes.ActiveQuiz}>
@@ -10,13 +11,7 @@ const ActiveQuiz = props => (
             </span>
             <small>4 из 12</small>
         </p>
-
-        <ul>
-            <li>1</li>
-            <li>2</li>
-            <li>3</li>
-            <li>4</li>
-        </ul>
+        <AnswersList answers={props.answers}/>
     </div>
 )
 
