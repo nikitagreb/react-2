c3624f320e4e3e0ee2dea5ace097e7ca4a678464
Nikita Grebennikov <nikitagreb0204@mail.ru>
2021-10-02 22:05:35 +0300

    7.76

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -5,8 +5,39 @@ import Input from './../../components/UI/Input/Input'
 
 export default class Auth extends Component {
 
-  submitHandler = () => {
+  state = {
+    formControls: {
+      email: {
+        value: '',
+        type: 'email',
+        label: 'Email',
+        errorMessage: 'Введите корректный email',
+        valid: false,
+        touched: false,
+        validation: {
+          required: true,
+          email: true
+        }
+      },
+      password: {
+        email: {
+          value: '',
+          type: 'password',
+          label: 'Пароль',
+          errorMessage: 'Введите корректный пароль',
+          valid: false,
+          touched: false,
+          validation: {
+            required: true,
+            minLength: 6
+          }
+        }
+      }
+    }
+  }
 
+  submitHandler = event => {
+    event.preventDefault()
   }
 
   loginHandler = () => {
@@ -17,6 +48,28 @@ export default class Auth extends Component {
 
   }
 
+  onChangeHandler = (event, controlName) => {
+    console.log(`${controlName}: `, event.target.value)
+  }
+
+  renderInputs() {
+    return Object.keys(this.state.formControls).map((controlName, index) => {
+      const control = this.state.formControls[controlName]
+      return (
+        <Input
+          key={controlName + index}
+          type={control.type}
+          value={control.value}
+          touched={control.touched}
+          label={control.label}
+          validation={!!control.validation}
+          errorMessage={control.errorMessage}
+          onChange={event => this.onChangeHandler(event, controlName)}
+        />
+      )
+    })
+  }
+
   render() {
     return (
       <div className={classes.Auth}>
@@ -24,16 +77,14 @@ export default class Auth extends Component {
           <h1>Авторизация</h1>
 
           <form className={classes.AuthForm} onSubmit={this.submitHandler}>
-            <Input label="Email" />
-            <Input label="Пароль" errorMessage='hhhhh' />
-
+            {this.renderInputs()}
             <Button
-              type='success'
+              type="success"
               onClick={this.loginHandler}>
               Войти
             </Button>
             <Button
-              type='primary'
+              type="primary"
               onClick={this.registerHandler}>
               Зарегистрироваться
             </Button>
